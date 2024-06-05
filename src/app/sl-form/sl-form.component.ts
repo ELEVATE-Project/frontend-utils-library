0eb17fec811c949f0df61f01dae1fd022972959e
LNU <2135664@cognizant.com>
2024-06-05 13:08:55 +0530

    issue

---
+++ src/app/sl-form/sl-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sl-form',
+  templateUrl: './sl-form.component.html',
+  styleUrls: ['./sl-form.component.css']
+})
+export class SlFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
