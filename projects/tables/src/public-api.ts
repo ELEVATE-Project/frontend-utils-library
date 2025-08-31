e8ac31e68d85f1aa2f2b902bac756a61a08b1863
vishwanath1004 <50612405+vishwanath1004@users.noreply.github.com>
2025-08-31 13:09:54 +0530

    generic chart and table

--- projects/tables/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of tables
- */
-
-export * from './lib/tables.service';
-export * from './lib/tables.component';
-export * from './lib/tables.module';
-export * from './lib/components/generic-table-comp/generic-table-comp.component'
