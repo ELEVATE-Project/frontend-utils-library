10b73d86dd32509fc3726c9bd39b112ce2b773ac
rohxnn <rohanreji123@gmail.com>
2026-03-05 14:55:51 +0530

    feat(generic-table): Migrate generic-chart and generic-table libraries to Angular 17 standalone components and new control flow syntax.ion

--- projects/generic-table/src/lib/generic-table.module.ts
+++ projects/generic-table/src/lib/generic-table.module.ts
@@ -1,45 +1,15 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { GenericTableComponent } from './generic-table.component';
 import { TableComponent } from './components/table/table.component';
-import { MatOptionModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { CommonModule } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
+
 @NgModule({
-  declarations: [
+  imports: [
     GenericTableComponent,
     TableComponent
   ],
-  imports: [
-    CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatOptionModule,
-    MatIconModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    FormsModule
-  ],
-  schemas:[NO_ERRORS_SCHEMA],
   exports: [
     GenericTableComponent,
     TableComponent
-  ],
-  providers:[DatePipe]
+  ]
 })
 export class GenericTableModule { }
